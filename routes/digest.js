55df235f9747e364f620da8377c005862c3dc5a8
maccooper <mackenziecooper99@gmail.com>
2025-04-23 19:07:28 -0700

    add story data fetching

--- routes/digest.js
+++ routes/digest.js
@@ -3,13 +3,19 @@ const router = express.Router();
 
 router.get('/', async (req,res) => {
 	try {
-		console.log('Fetching top stories...');
+		//Step 1: Fetch top story IDs
+
 		const response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
 		const ids = await response.json();
+		const top5 = ids.slice(0,5);
 
-		console.log('Got IDs:', ids.slice(0,5));
-		res.json(ids.slice(0,5));
+		const stories = await Promise.all(
+			top5.map(id =>
+				fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(res => res.json())
+			)
+		);
 
+		res.json(stories);
 		} catch (err) {
 			console.error('Fetch error:', err.message);
 			res.status(500).json({ error: 'Failed to fetch story' });
