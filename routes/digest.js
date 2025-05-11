cc21c33a13578ae1cf6f8161c2f28f9c6c468afc
Mac Cooper <mackenzie.cooper99@gmail.com>
2025-05-11 06:59:33 -0700

    change api item slice from 5 to 100

--- routes/digest.js
+++ routes/digest.js
@@ -3,14 +3,13 @@ const router = express.Router();
 
 router.get('/', async (req,res) => {
 	try {
-		//Step 1: Fetch top story IDs
 
 		const response = await fetch('https://hacker-news.firebaseio.com/v0/topstories.json');
 		const ids = await response.json();
-		const top5 = ids.slice(0,10);
+		const top_n = ids.slice(0,100);
 
 		const stories = await Promise.all(
-			top5.map(id =>
+			top_n.map(id =>
 				fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`).then(res => res.json())
 			)
 		);
